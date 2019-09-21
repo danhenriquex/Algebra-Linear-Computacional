4a88e3fafae6ea74c32c2df752c76e89cf1f5124
Danilo Henrique <45819060+danhenriquex@users.noreply.github.com>
2019-09-21 09:28:50 -0300

    Update elimina_zeros.m

--- prova_2/elimina_zeros.m
+++ prova_2/elimina_zeros.m
@@ -1,10 +1,10 @@
-function p = elimina_zeros(p)
-  n = size(p,1);
-  p1 = [];
-  for i = 1:n
-    if(p(i) != 0)
-      p1 = [p1 p(i)];
-    endif
-  endfor
-  p = p1;
-endfunction
+ function p = elimina_zeros(p)
+   n = size(p,1);
+   p1 = [];
+   for i = 1:n
+     if(p(i) != 0)
+       p1 = [p1 p(i)];
+     endif
+   endfor
+   p = p1;
+ endfunction
